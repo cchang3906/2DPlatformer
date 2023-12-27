b7c84ef2522b03f6e5f7aa17c71160e6bd3774a8
cchang3906 <cchang3906@gmail.com>
2023-12-28 00:31:14 +0900

    final

---
+++ Assets/Scripts/Pickups/FullHealPickup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullHealPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //full heal on collision
+        if(collision.tag=="Player")
+        {
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+            playerHealth.ReceiveHealing(playerHealth.maximumHealth);
+            Destroy(this.gameObject);
+        }
+    }
+}
